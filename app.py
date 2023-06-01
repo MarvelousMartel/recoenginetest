5cc4b0be2c7ea2cd6ca818ccee8ed70b013dab13
MarvelousMartel <109018241+MarvelousMartel@users.noreply.github.com>
2023-06-01 16:13:44 +0800

    bug fix 2

--- app.py
+++ app.py
@@ -16,9 +16,6 @@ features = data[['review_count', 'price', 'latitude', 'longitude']]
 features['price'] = features['price'].astype(str)  # Convert price to string
 feature_matrix = features.values
 
-# Convert feature_matrix to a NumPy array
-feature_matrix = np.asarray(feature_matrix)
-
 # Calculate cosine similarity
 similarity_matrix = cosine_similarity(feature_matrix, feature_matrix)
 
@@ -26,37 +23,31 @@ similarity_matrix = cosine_similarity(feature_matrix, feature_matrix)
 def get_recommendations(restaurant_name, num_recommendations):
     # Get the index of the restaurant with the given name
     index = data[data['name'] == restaurant_name].index[0]
-
+    
     # Get the similarity scores for the given restaurant
     similarity_scores = similarity_matrix[index]
-
-    # Sort the restaurants based on similarity scores
-    sorted_indices = similarity_scores.argsort()[::-1]
     
-    # Exclude the restaurant itself from recommendations
-    sorted_indices = sorted_indices[1:]
-
-    # Get the top N recommendations
-    top_indices = sorted_indices[:num_recommendations]
-
-    # Get the restaurant names of the top recommendations
-    recommendations = data.iloc[top_indices]['name'].values
-
-    return recommendations
+    # Get the indices of the top recommendations
+    top_indices = similarity_scores.argsort()[-num_recommendations - 1:-1][::-1]
+    
+    # Get the names of the top recommendations
+    top_recommendations = data.loc[top_indices, 'name']
+    
+    return top_recommendations.tolist()
 
 # Streamlit app
-def main():
-    st.title("Restaurant Recommendation System")
-    
-    # Dropdown for restaurant name selection
-    restaurant_name = st.selectbox("Select a restaurant:", data['name'].values)
+st.title("Restaurant Recommendation Engine")
 
-    num_recommendations = 5
-    if st.button("Get Recommendations"):
-        recommendations = get_recommendations(restaurant_name, num_recommendations)
-        st.subheader("Top Recommendations:")
-        for i, recommendation in enumerate(recommendations):
-            st.write(f"{i+1}. {recommendation}")
+# Dropdown for selecting a restaurant
+restaurant_name = st.selectbox("Select a restaurant:", data['name'].tolist())
 
-if __name__ == "__main__":
-    main()
+# Number of recommendations
+num_recommendations = st.slider("Number of recommendations:", min_value=1, max_value=10, value=5)
+
+# Get recommendations
+recommendations = get_recommendations(restaurant_name, num_recommendations)
+
+# Display recommendations
+st.subheader("Top Recommendations:")
+for i, recommendation in enumerate(recommendations):
+    st.write(f"{i+1}. {recommendation}")
