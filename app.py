012a77617ae3a83fc426b6a8b30e0a67db57947b
MarvelousMartel <109018241+MarvelousMartel@users.noreply.github.com>
2023-06-01 16:24:39 +0800

    bug fix 3

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-import streamlit as st
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import OneHotEncoder
 
 # Load the dataset
 data = pd.read_csv('01_MMla_with_reviews.csv')
 
-# Drop rows with missing values
-data.dropna(inplace=True)
+# Perform one-hot encoding for the 'price_bucket' column
+encoder = OneHotEncoder(sparse=False)
+price_bucket_encoded = encoder.fit_transform(data['price_bucket'].values.reshape(-1, 1))
+encoded_columns = encoder.categories_[0]
 
-# Feature representation
-# Create a feature matrix using rating, review_count, and price_bucket
-features = data[['rating', 'review_count', 'price_bucket']]
-feature_matrix = features.values
+# Create the feature matrix
+features = data[['rating', 'review_count']]
+feature_matrix = pd.DataFrame(features.values, columns=features.columns)
+feature_matrix = pd.concat([feature_matrix, pd.DataFrame(price_bucket_encoded, columns=encoded_columns)], axis=1)
 
 # Calculate cosine similarity
 similarity_matrix = cosine_similarity(feature_matrix, feature_matrix)
@@ -25,27 +26,20 @@ def get_recommendations(restaurant_name, num_recommendations):
     # Get the similarity scores for the given restaurant
     similarity_scores = similarity_matrix[index]
     
-    # Get the indices of the top recommendations
-    top_indices = similarity_scores.argsort()[-num_recommendations - 1:-1][::-1]
+    # Sort the restaurants based on similarity scores
+    sorted_indices = similarity_scores.argsort()[::-1]
     
-    # Get the names of the top recommendations
-    top_recommendations = data.loc[top_indices, 'name']
+    # Exclude the input restaurant itself
+    sorted_indices = sorted_indices[1:]
     
-    return top_recommendations.tolist()
+    # Get the top N recommendations
+    top_indices = sorted_indices[:num_recommendations]
+    recommendations = data.loc[top_indices, 'name']
+    
+    return recommendations
 
-# Streamlit app
-st.title("Restaurant Recommendation Engine")
-
-# Dropdown for selecting a restaurant
-restaurant_name = st.selectbox("Select a restaurant:", data['name'].tolist())
-
-# Number of recommendations
-num_recommendations = st.slider("Number of recommendations:", min_value=1, max_value=10, value=5)
-
-# Get recommendations
+# Example usage
+restaurant_name = 'Your Restaurant Name'
+num_recommendations = 5
 recommendations = get_recommendations(restaurant_name, num_recommendations)
-
-# Display recommendations
-st.subheader("Top Recommendations:")
-for i, recommendation in enumerate(recommendations):
-    st.write(f"{i+1}. {recommendation}")
+print(recommendations)
