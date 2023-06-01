f24a1d835480647dbca38e14e99ac2c3df867f0e
MarvelousMartel <109018241+MarvelousMartel@users.noreply.github.com>
2023-06-01 16:06:57 +0800

    v3

--- app.py
+++ app.py
@@ -1,60 +1,58 @@
-import pandas as pd
 import streamlit as st
+import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-# Load the restaurant data into a pandas DataFrame
-data = pd.read_csv('01_MMla_with_reviews.csv')
-
-# Preprocessing
-# Clean the data (e.g., remove irrelevant columns)
-data = data[['id', 'name', 'review_count', 'categories', 'rating', 'price', 'city', 'latitude', 'longitude']]
+# Load the dataset
+data = pd.read_csv('your_dataset.csv')
 
 # Drop rows with missing values
 data.dropna(inplace=True)
 
 # Feature representation
-# Convert restaurant names to feature vectors using TF-IDF vectorization
-vectorizer = TfidfVectorizer()
-name_features = vectorizer.fit_transform(data['name'])
+# Create a feature matrix using review count, price, latitude, and longitude
+features = data[['review_count', 'price', 'latitude', 'longitude']]
+features['price'] = features['price'].astype(str)  # Convert price to string
+feature_matrix = features.values
 
-# Calculate similarity
-# Calculate cosine similarity between restaurant names
-similarity_matrix = cosine_similarity(name_features, name_features)
+# Calculate cosine similarity
+similarity_matrix = cosine_similarity(feature_matrix, feature_matrix)
 
-# Define the Streamlit app
-def main():
-    st.title('Restaurant Recommender')
-    
-    # Dropdown menu for restaurant names
-    restaurant_names = data['name'].tolist()
-    selected_name = st.selectbox('Select a restaurant name:', restaurant_names)
-    
-    num_recommendations = st.slider('Number of recommendations:', 1, 10, 5)
-    
-    # Get recommendations based on selected name
-    recommendations = get_recommendations(selected_name, num_recommendations)
-    
-    # Display the recommendations
-    st.subheader('Recommended Restaurants:')
-    st.table(recommendations[['name', 'categories', 'rating', 'price', 'city']])
-    
-# Function to get restaurant recommendations
+# Define the function to get restaurant recommendations
 def get_recommendations(restaurant_name, num_recommendations):
-    # Get the index of the restaurant
+    # Get the index of the restaurant with the given name
     index = data[data['name'] == restaurant_name].index[0]
-    
+
     # Get the similarity scores for the given restaurant
     similarity_scores = similarity_matrix[index]
-    
-    # Sort the restaurants based on similarity scores
-    similar_restaurants_indices = similarity_scores.argsort()[::-1][1:]  # Exclude the current restaurant
-    
-    # Get the top similar restaurants
-    top_similar_restaurants = data.iloc[similar_restaurants_indices][:num_recommendations]
-    
-    return top_similar_restaurants
 
-# Run the Streamlit app
-if __name__ == '__main__':
+    # Sort the restaurants based on similarity scores
+    sorted_indices = similarity_scores.argsort()[::-1]
+    
+    # Exclude the restaurant itself from recommendations
+    sorted_indices = sorted_indices[1:]
+
+    # Get the top N recommendations
+    top_indices = sorted_indices[:num_recommendations]
+
+    # Get the restaurant names of the top recommendations
+    recommendations = data.iloc[top_indices]['name'].values
+
+    return recommendations
+
+# Streamlit app
+def main():
+    st.title("Restaurant Recommendation System")
+    
+    # Dropdown for restaurant name selection
+    restaurant_name = st.selectbox("Select a restaurant:", data['name'].values)
+
+    num_recommendations = 5
+    if st.button("Get Recommendations"):
+        recommendations = get_recommendations(restaurant_name, num_recommendations)
+        st.subheader("Top Recommendations:")
+        for i, recommendation in enumerate(recommendations):
+            st.write(f"{i+1}. {recommendation}")
+
+if __name__ == "__main__":
     main()
